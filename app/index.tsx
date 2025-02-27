0d8bacb94734549dc68931f2222cd2ff3d6189b7
MathMarcondes <matheus.marcondes@outlook.com>
2025-02-27 13:24:06 -0300

    Component: Header

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+import Home from "./src/pages/Home";
+
+
+export default function Page() {
+  return (
+    <View>
+      <Home/>
+      
+    </View>
+  );
+}
+
---
+++ app/src/pages/Home/index.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import Header from "../../Components/Header";
+
+
+export default function Home() {
+  return (
+    <View>
+      <Header name="Matheus Marcondes"/>
+      <Text>Página Home</Text>
+    </View>
+    
+  );
+}
+
+
